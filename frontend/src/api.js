9cbc81940f4744e28d9174b242fc7da9302aeed9
Manoj2571 <manojreddy2571@gmail.com>
2025-07-05 14:40:09 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'https://resource-management-system-nine.vercel.app/api'
 })
 
 export default api
